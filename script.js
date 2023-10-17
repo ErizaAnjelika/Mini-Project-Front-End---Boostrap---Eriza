52561508f4e0f922e826a34976f32a5bce52a1c9
ErizAnjelika <erizaangel5@gmail.com>
2023-10-17 20:27:24 +0700

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Bagian Navbar
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) { 
-    navbar.classList.add('blur'); 
+  if (window.scrollY > 100) {
+    navbar.classList.add('blur');
   } else {
-    navbar.classList.remove('blur'); 
+    navbar.classList.remove('blur');
   }
 });
 
@@ -22,49 +22,82 @@ window.addEventListener('scroll', () => {
 scrollToTopButton.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
 });
 
-// bagian quantity
+// Mendapatkan elemen total harga
+const modalTotalHarga = document.getElementById('modal-harga-produk');
 const modalQuantityInput = document.getElementById('examplequantity');
 const modalIncrementButton = document.getElementById('incrementButton');
 const modalDecrementButton = document.getElementById('decrementButton');
 
-// Menambahkan event listener untuk tombol Increment di dalam modal
+let hargaPerProduk = 0; // Inisialisasi harga per produk
+
+// Fungsi untuk menghitung dan memperbarui total harga
+function updateTotalHarga() {
+  const quantity = parseInt(modalQuantityInput.value);
+  const totalHarga = hargaPerProduk * quantity;
+  modalTotalHarga.textContent = `Rp${totalHarga}`;
+}
+
+// Bagian quantity
 modalIncrementButton.addEventListener('click', function () {
   modalQuantityInput.value = parseInt(modalQuantityInput.value) + 1;
+  updateTotalHarga();
 });
 
-// Menambahkan event listener untuk tombol Decrement di dalam modal
 modalDecrementButton.addEventListener('click', function () {
   const currentValue = parseInt(modalQuantityInput.value);
-  if (currentValue > 0) { 
+  if (currentValue > 0) {
     modalQuantityInput.value = currentValue - 1;
+    updateTotalHarga();
   }
 });
 
-// untuk menampilkan nama dan harga pesanan
-  const pesanButtons = document.querySelectorAll('.pesan-button');
-  const modalNamaProduk = document.getElementById('modal-nama-produk');
-  const modalHargaProduk = document.getElementById('modal-harga-produk');
-  const modalGambarProduk = document.getElementById('modal-gambar-produk');
-
+// Untuk menampilkan nama dan harga pesanan
+const pesanButtons = document.querySelectorAll('.pesan-button');
+const modalNamaProduk = document.getElementById('modal-nama-produk');
+const modalGambarProduk = document.getElementById('modal-gambar-produk');
 
 pesanButtons.forEach((button) => {
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function () {
     const card = this.closest('.card');
     const namaProduk = card.getAttribute('data-nama');
-    const hargaProduk = card.getAttribute('data-harga');
     const gambarProduk = card.getAttribute('data-gambar');
+
+    // Mengambil harga per produk dari atribut HTML
+    hargaPerProduk = parseFloat(card.getAttribute('data-harga'));
 
     // Perbarui modal dengan nama dan harga produk yang sesuai
     modalNamaProduk.textContent = namaProduk;
-    modalHargaProduk.textContent = `Rp${hargaProduk}`;
     modalGambarProduk.src = gambarProduk;
+
+    // Set nilai quantity ke 0 dan perbarui total harga
+    modalQuantityInput.value = 0;
+    updateTotalHarga();
   });
 });
 
+// Alert success
+document.addEventListener('DOMContentLoaded', function () {
+  const pesanSekarangBtn = document.getElementById('pesanSekarangBtn');
+  const popupMessage = document.getElementById('popupMessage');
+  const namaInput = document.getElementById('examplenama');
+  const telpInput = document.getElementById('exampletelp');
+  const alamatTextarea = document.getElementById('examplealamat');
+  const quantityInput = document.getElementById('examplequantity');
 
+  pesanSekarangBtn.addEventListener('click', function () {
+    if (namaInput.value && telpInput.value && alamatTextarea.value && quantityInput.value > 0) {
+      // Set pesan yang akan ditampilkan pada jendela pop-up
+      popupMessage.textContent = 'Pesanan Anda Segera kami Proses! Tunggu Pesanan Anda sampai';
 
-
+      // Tampilkan jendela pop-up
+      const popup = new bootstrap.Modal(document.getElementById('popup'));
+      popup.show();
+    } else {
+      alert('Harap isi semua elemen formulir terlebih dahulu!');
+    }
+  });
+});
